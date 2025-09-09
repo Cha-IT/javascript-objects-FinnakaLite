d0808cbdfe78d78d4256626c2d0de9287caab60e
FinnakaLite <Finng2008@outlook.com>
2025-09-09 14:48:26 +0200

    1a til 1d

---
+++ app.js
@@ -0,0 +1,19 @@
+let informasjonsteknologi1 = {
+    spraak: ["HTML", "CSS"],
+    likerFaget: false,
+    timetall: 5,
+    klasserom: "c14",
+    antallElever: 24
+};
+
+// 1a
+informasjonsteknologi1.spraak.push("JavaScript");
+
+// 1b
+informasjonsteknologi1.timetall = 67;
+
+//1c
+informasjonsteknologi1.likerFaget = true;
+
+//1d
+delete informasjonsteknologi1.timetall;
